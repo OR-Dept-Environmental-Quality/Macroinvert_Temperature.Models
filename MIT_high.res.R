86c649e1891d69743b2123b6bdeca03d315a8c52
ShannonHubler <hubler.shannon@deq.state.or.us>
2022-01-06 17:06:52 -0800

    more bias plots

--- MIT_high.res.R
+++ MIT_high.res.R
@@ -172,7 +172,7 @@ env.val <- val %>%
 	distinct_all()
 
 
-env.val <-	column_to_rownames(env.cal, 'site.id')
+env.val <-	column_to_rownames(env.val, 'site.id')
 
 
 hist(env.val$MWMT_final)
@@ -269,39 +269,223 @@ plot(wa_high.res, resid=TRUE, xval=FALSE, tolDW=FALSE, deshrink="classical",
 				plot(wa_high.res_RA.trans, resid=TRUE, xval=FALSE, tolDW=FALSE, deshrink="classical",
 					  xlab="MWMT", ylab="residuals", ylim=c(-15,15), xlim=c(0,35), add.ref=TRUE,
 					  add.smooth=TRUE)
-
+######
+				
 # make predictions for VAL dataset				
+				
+######
 				
 wa_high.res_VAL <- predict(wa_high.res, newdata=bug.val_high_wide, sse=TRUE, nboot=100,
 									match.data=TRUE, verbose=TRUE)
 
-# get RMSE for VAL data
-wa_high.res_VAL$v2.boot 
-wa_high.res_VAL$fit
-			@@@@ whats the difference between fit and fit.boot?
+#### get RMSE and r2 for VAL data
+
+
+# extract VAL predictions as a standalone dataframe, so can join with env.val and calculate RMSEP
+VAL.predicts <- as.data.frame(wa_high.res_VAL$fit)
+VAL.predicts <- cbind(VAL.predicts, env.val)
 	
 
-@@@@@ how is this giving the RMSE for VAL?  You need ENV and Inferences to calculate that...'predict' above
-@@@@@ doesnt include ENV....????
-				
+
+VAL.rmsep <- VAL.predicts %>%
+	mutate(VAL_WA.inv_rmsep = sqrt(mean((VAL.predicts$WA.inv - VAL.predicts$MWMT_final)^2))) %>%
+	mutate(VAL_WA.cla_rmsep = sqrt(mean((VAL.predicts$WA.cla - VAL.predicts$MWMT_final)^2)))	%>%	 
+	mutate(VAL_WA.m_rmsep = sqrt(mean((VAL.predicts$WA.m - VAL.predicts$MWMT_final)^2)))		 %>%
+	mutate(VAL_WA.inv.tol_rmsep = sqrt(mean((VAL.predicts$WA.inv.tol - VAL.predicts$MWMT_final)^2)))	%>%	 
+	mutate(VAL_WA.cla.tol_rmsep = sqrt(mean((VAL.predicts$WA.cla.tol - VAL.predicts$MWMT_final)^2)))	%>%	 
+	mutate(VAL_WA.m.tol_rmsep = sqrt(mean((VAL.predicts$WA.m.tol - VAL.predicts$MWMT_final)^2)))	%>%
+	mutate(VAL.WA.inv_rsq = cor(VAL.predicts$WA.inv, VAL.predicts$MWMT_final)^2) 	%>%
+	mutate(VAL.WA.cla_rsq = cor(VAL.predicts$WA.cla, VAL.predicts$MWMT_final)^2)		%>%
+	mutate(VAL.WA.m_rsq = cor(VAL.predicts$WA.m, VAL.predicts$MWMT_final)^2)			%>%
+	mutate(VAL.WA.inv.tol_rsq = cor(VAL.predicts$WA.inv.tol, VAL.predicts$MWMT_final)^2)	%>%
+	mutate(VAL.WA.cla.tol_rsq = cor(VAL.predicts$WA.cla.tol, VAL.predicts$MWMT_final)^2)	%>%
+	mutate(VAL.WA.m.tol_rsq = cor(VAL.predicts$WA.m.tol, VAL.predicts$MWMT_final)^2)
+
+VAL.rmsep <- VAL.rmsep[,8:19]
+VAL.rmsep <- distinct(VAL.rmsep)
+row.names(VAL.rmsep) <- NULL
+
+
+######
+
+#			Look for potential BIAS in predictions - natural gradients, disturbance, etc.
+
+#####
+					
 # join MWMT and Inferred values, plus site specific data
 
-site.data <- read.csv('site.data_PrimVal_JS.csv')
+site.data <- read.csv('SiteData_wCollDate_20220106.csv')
+
+
+
 site.data <- site.data %>%
 				rename('sample.id' = 'UniqueID_v2')
+			
 
+
+eco.cnt <- site.data %>%
+	group_by(calval, eco3) %>%
+	summarize(count = n()) %>%
+	pivot_wider(names_from = calval, values_from = count)
+
+# numerical summaries -- need to remove categorical vars
+site.data_num <- site.data %>%
+	select(sample.id, siteid, calval, daynum, Year, MWMT_final, lat, long, slope_nhd, wsarea_km2, elev_m, IWI, PctForCat, PctForWs)
+
+site.data_num.long <- site.data_num %>%
+	pivot_longer(!c(sample.id, siteid, calval)  , names_to = "variable", values_to = "value")
+
+
+sum.stats <- site.data_num.long %>%                               # Summary by group using dplyr
+					group_by(calval, variable) %>% 
+					summarize(min = min(value),
+								 q1 = quantile(value, 0.25, na.rm=TRUE),
+								 median = median(value, na.rm=TRUE),
+								 mean = mean(value, na.rm=TRUE),
+								 q3 = quantile(value, 0.75, na.rm=TRUE),
+								 max = max(value, na.rm=TRUE))
+
+@@@@ mostly works--why is MIN not working?
+
+	
+# boxplots
+	site.data_num.long %>% 
+	ggplot(aes(x= calval, y=value, fill=calval)) +
+	geom_boxplot() +
+	#geom_jitter(width=0.1,alpha=0.2) +
+	xlab("")+ 
+	facet_wrap(~variable, scales = "free") +
+	theme(axis.text.x = element_text(angle = 45, hjust = 1))
+	
+
+# PCA
+
+library(factoextra)
+
+# limit dframe to numeric variables = use 'site.data_num'
+
+								# remove columns with "zero variance"
+								#which(apply(site.data_num, 2, var)==0)  
+# remove incomplete cases
+site.data_pca <- site.data[complete.cases(site.data), ]
+		# missing data blows up the PCA -- remove rows with missing data
+								#site.data_complete <- site.data_complete %>% select(!PctForWs) # highly correlated with 'PctForCat'
+
+				# data transformations   ????
+				slope, elev = sqrt
+				wsarea = log10
+
+
+								#rownames(site.data_complete) <- site.data_complete[,1]
+								#site.data_pca <- site.data_complete[,-c(1:3)]
+
+
+# run pca 
+library(FactoMineR)  
+library("factoextra")
+library("corrplot")
+
+
+pca.allsites <- PCA(site.data_pca[,c(4,8, 11:17)], graph = FALSE)
+
+summary(pca.allsites)
+str(pca.allsites)
+print(pca.allsites)
+eig.val <- get_eigenvalue(pca.allsites) # Extract the eigenvalues/variances of principal components
+fviz_eig(pca.allsites, addlabels = TRUE, ylim = c(0, 50) ) # Visualize the eigenvalues
+
+# Extract the results for individuals and variables, respectively.
+ind <- get_pca_ind(pca.allsites); 
+var <- get_pca_var(pca.allsites) 
+
+# Visualize the results individuals and variables, respectively.
+fviz_pca_ind(pca.allsites)
+fviz_pca_var(pca.allsites, col.var = "blue") 
+
+fviz_pca_biplot(pca.allsites) # Make a biplot of individuals and variables.
+
+
+corrplot(var$cos2, is.corr=FALSE)  # ???????
+
+# Total cos2 of variables on Dim.1 and Dim.2
+fviz_cos2(pca.allsites, choice = "var", axes = 1:2)
+
+
+# Color by cos2 values: quality on the factor map
+fviz_pca_var(pca.allsites, col.var = "cos2",
+				 gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"), 
+				 repel = TRUE # Avoid text overlapping
+)
+
+
+fviz_pca_var(pca.allsites, alpha.var = "cos2") # Change the transparency by cos2 values
+
+corrplot(var$contrib, is.corr=FALSE)  
+
+fviz_contrib(pca.allsites, choice = "var", axes = 1, top = 15)
+fviz_contrib(pca.allsites, choice = "var", axes = 2, top = 15)
+fviz_contrib(pca.allsites, choice = "var", axes = 1:2, top = 15)
+
+
+# The most important (or, contributing) variables can be highlighted on the correlation plot as follow:
+fviz_pca_var(pca.allsites, col.var = "contrib",
+				 gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07")
+)
+
+
+fviz_pca_ind(pca.allsites, col.ind = "cos2", 
+				 gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),
+				 repel = TRUE # Avoid text overlapping (slow if many points)
+)
+
+
+grp <- as.factor(site.data_pca$calval)
+# Color variables by groups
+fviz_pca_var(pca.allsites, col.var = grp, 
+				 palette = c("#0073C2FF", "#EFC000FF"),
+				 legend.title = "Cluster")
+
+
+fviz_pca_ind(pca.allsites,
+				 col.ind = grp, # color by groups
+				 palette = c("#00AFBB",  "#FC4E07"),
+				 addEllipses = FALSE, # Concentration ellipses
+				 ellipse.type = "confidence",
+				 legend.title = "Groups",
+				 repel = TRUE
+)
+
+@@@ how to unlabel all points (individuals)?
+	
+@@@ how to add variable biplot as well?  It owuld be nice to see what is driving the sites in upper-left.
+
+
+
+
+
+
+
+
+
+
+
+######
+
+# BIAS = look for patterns in CAL predictions--do we see bias associated with other variables?
+			@@@@ do we need to look at VAL as well?
+######
 
 site.data_residuals <- site.data %>%
 							 inner_join(WA.resid.high, by = c('sample.id'))
 
 
-site.data_residuals$eco3 <- plyr::revalue(site.data_residuals$eco3, c("Blue Mountains" = "B.Mts",                                   
-			"Cascades" = "Casc", "Coast Range" = "Co.Ra", "Columbia Plateau" = "Col.Pl",
-			"Eastern Cascades Slopes and Foothills" = "E.Casc",
-			"Klamath Mountains/California High North Coast Range" = "Kl.Mts", 
-			"North Cascades" = "N.Casc", "Northern Basin and Range" = "N.Ba.Ra",
-			"Northern Rockies" = "N.Rock", "Puget Lowland" = "Pu.Low",
-			"Snake River Plain" = "Sna.Riv.Pl", "Willamette Valley" = "Wil.Val"))     
+										# site.data_residuals$eco3 <- plyr::revalue(site.data_residuals$eco3, c("Blue Mountains" = "B.Mts",                                   
+										# 			"Cascades" = "Casc", "Coast Range" = "Co.Ra", "Columbia Plateau" = "Col.Pl",
+										# 			"Eastern Cascades Slopes and Foothills" = "E.Casc",
+										# 			"Klamath Mountains/California High North Coast Range" = "Kl.Mts", 
+										# 			"North Cascades" = "N.Casc", "Northern Basin and Range" = "N.Ba.Ra",
+										# 			"Northern Rockies" = "N.Rock", "Puget Lowland" = "Pu.Low",
+										# 			"Snake River Plain" = "Sna.Riv.Pl", "Willamette Valley" = "Wil.Val"))     
 	
 # residuals ~ longitude
 p <- ggplot(data=site.data_residuals, aes(x=long, y=WA.resid_high))
@@ -333,6 +517,15 @@ p + geom_point()	+ geom_smooth(method="lm")
 p <- ggplot(data=site.data_residuals, aes(x=IWI, y=WA.resid_high))
 p + geom_point()		+ geom_smooth(method="lm")				
 
+# residuals ~ daynum
+p <- ggplot(data=site.data_residuals, aes(x=daynum, y=WA.resid_high))
+p + geom_point()		+ geom_smooth(method="lm")				
+
+
+# residuals ~ year
+p <- ggplot(data=site.data_residuals, aes(x=Year, y=WA.resid_high))
+p + geom_point()		+ geom_smooth(method="lm")				
+
 
 
 # residuals ~ eocregions
@@ -340,7 +533,11 @@ p <- ggplot(data=site.data_residuals, aes(x=eco3, y=WA.resid_high))
 p + geom_boxplot() + theme(axis.text.x=element_text(size=8, angle = 0, vjust=.5))+
 	geom_hline(yintercept=0, linetype="dashed", color = "red") 
 								 
-								 
+# residuals ~ Year
+p <- ggplot(data=site.data_residuals, aes(x=as.factor(Year), y=WA.resid_high))								 
+p + geom_boxplot() + theme(axis.text.x=element_text(size=8, angle = 0, vjust=.5))+
+	geom_hline(yintercept=0, linetype="dashed", color = "red") 
+
 							 
 #####
 
@@ -398,17 +595,17 @@ segmentwise.rmse(modMAT, k=5, main="MAT", xlab="pH")
 ####
 
 # need to convert env to vector, not dataframe
-env.num <- as.numeric(unlist(env))
 
 
-IKFA(y=spec, x=env.num, nFact = 5, IsPoly = FALSE, IsRot = TRUE,
+
+IKFA(y=spec, x=env.cal, nFact = 5, IsPoly = FALSE, IsRot = TRUE,
 	  ccoef = 1:nFact, check.data=TRUE, lean=FALSE)
 		# results: 4 factors to get RMSE similar to WA.  R2 poor.  Max bias high.
 
-MAT(y=spec, x=env.num, dist.method="bray", k=5, lean=TRUE)
+MAT(y=spec, x=env.cal, dist.method="bray", k=5, lean=TRUE)
 		# results: RMSE similar to WA.  R2 equivalent at NO4.  Max bias equal to INV deshrinking.
 
-mrlc <- MLRC(y=spec, x=env.num, check.data=TRUE, lean=FALSE, n.cut=5, verbose=TRUE)
+mrlc <- MLRC(y=spec, x=env.cal, check.data=TRUE, lean=FALSE, n.cut=5, verbose=TRUE)
 		# results: RMSE similar to WA.  R2 similar.  Max bias lower than WA-cla (2.8)
 
 	summary(mrlc)
@@ -416,13 +613,13 @@ mrlc <- MLRC(y=spec, x=env.num, check.data=TRUE, lean=FALSE, n.cut=5, verbose=TR
 	crossval(mrlc, cv.method="lgo", verbose=TRUE, ngroups=10,
 				nboot=100, h.cutoff=0, h.dist=NULL)
 
-mr <- MR(y=spec, x=env.num, check.data=TRUE, lean=FALSE)
+mr <- MR(y=spec, x=env.cal, check.data=TRUE, lean=FALSE)
 		# RMSE a fair bit lower.  R2 a bit higher.  But max bias = WA.inv
 	summary(mr)
 	plot(mr, add.ref = TRUE, add.smooth = TRUE) # bias at extremes
 
 
-wapls <-	WAPLS(y=spec, x=env.num, npls=5, iswapls=TRUE, standx=FALSE, lean=FALSE,
+wapls <-	WAPLS(y=spec, x=env.cal, npls=5, iswapls=TRUE, standx=FALSE, lean=FALSE,
 			check.data=TRUE)
 		# results: RMSE improves with 3 or more, R2 improves a bit.  Max bias = WA.inv
 		summary(wapls)
